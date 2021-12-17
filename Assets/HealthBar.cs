bc35847e9333b39d5d0bd15f081b070e07bc073c
Ivan B <sensenabi@gmail.com>
2021-12-17 18:11:05 -0500

    more ui

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -6,13 +6,19 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     [SerializeField] private Slider slider;
+    [SerializeField] private Text text;
     public void setMaxHealth(int health)
     {
         slider.maxValue = health;
         slider.value = health;
+        if (text != null)
+        {
+            text.text = health.ToString();
+        }
     }
     public void setHealth(int health)
     {
         slider.value = health;
+        text.text = health.ToString();
     }
 }
